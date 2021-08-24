6bd119c5fa5a2908c9927cf6bc44299556cf9960
gustavo867 <gustavosantanabarbosa867@gmail.com>
2021-08-24 08:46:50 -0300

    :tada: finishing challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    setTasks((state) => [
+      ...state,
+      { id: new Date().getTime(), done: false, title: newTaskTitle },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const findTask = tasks.find((item) => item.id === id);
+
+    if (findTask) {
+      setTasks((state) =>
+        state.map((item) =>
+          item.id === id ? { ...item, done: item.done ? false : true } : item
+        )
+      );
+    }
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const findTask = tasks.find((item) => item.id === id);
+
+    if (findTask) {
+      setTasks((state) => state.filter((item) => item.id !== id));
+    }
   }
 
   return (
@@ -26,18 +44,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
